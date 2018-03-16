9652545fc7ca7c9d929cae116eccae3cab5ba03d
GBTC406006UR <GBTC406006UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-16 14:27:11 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBooy11Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooy11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBooy11Application.class, args);
+	}
+}
